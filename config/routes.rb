b6d0b3eee6e6a2604feebcc17718985bef9f4ae8
tonegawa07 <fukudayusuke07@gmail.com>
2021-01-12 19:00:18 +0900

    home,about,helpを追加．UIを改善．

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root 'icp_reservations#index'
+  root 'static_pages#home'
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
   resources :cn_reservations
   resources :icp_reservations
 end
