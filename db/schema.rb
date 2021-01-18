df153464646af3c7f422ae41cf046e5bdd12e4d4
tonegawa07 <fukudayusuke07@gmail.com>
2021-01-18 18:50:21 +0900

    Userモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_12_040426) do
+ActiveRecord::Schema.define(version: 2021_01_18_092109) do
 
   create_table "cn_reservations", force: :cascade do |t|
     t.string "lab"
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(version: 2021_01_12_040426) do
     t.datetime "start_time"
     t.datetime "end_time"
     t.text "information"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
