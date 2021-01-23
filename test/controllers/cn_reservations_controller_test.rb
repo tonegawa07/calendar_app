7821f3121749a8289e8eb5c97b646f0c76158215
tonegawa07 <fukudayusuke07@gmail.com>
2021-01-23 17:49:57 +0900

    minitestでログインしているかしていないかのテスト

--- test/controllers/cn_reservations_controller_test.rb
+++ test/controllers/cn_reservations_controller_test.rb
@@ -1,21 +1,30 @@
 require 'test_helper'
 
 class CnReservationsControllerTest < ActionDispatch::IntegrationTest
-  setup do
+  
+  def setup
     @cn_reservation = cn_reservations(:one)
   end
 
-  test "should get index" do
+  test "should get index when not logged in" do
+    get cn_reservations_path
+    assert_redirected_to login_url
+  end
+
+  test "should get index when logged in" do
+    log_in_as("password")
     get cn_reservations_url
     assert_response :success
   end
 
-  test "should get new" do
+  test "should get new when logged in as admin" do
+    log_in_as("password")
     get new_cn_reservation_url
     assert_response :success
   end
 
   test "should create cn_reservation" do
+    log_in_as("password")
     assert_difference('CnReservation.count') do
       post cn_reservations_url, params: { cn_reservation: { end_time: @cn_reservation.end_time, information: @cn_reservation.information, lab: @cn_reservation.lab, name: @cn_reservation.name, number_of_samples: @cn_reservation.number_of_samples, start_time: @cn_reservation.start_time } }
     end
@@ -24,21 +33,25 @@ class CnReservationsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should show cn_reservation" do
+    log_in_as("password")
     get cn_reservation_url(@cn_reservation)
     assert_response :success
   end
 
-  test "should get edit" do
+  test "should get edit when logged in as admin" do
+    log_in_as("password")
     get edit_cn_reservation_url(@cn_reservation)
     assert_response :success
   end
 
   test "should update cn_reservation" do
+    log_in_as("password")
     patch cn_reservation_url(@cn_reservation), params: { cn_reservation: { end_time: @cn_reservation.end_time, information: @cn_reservation.information, lab: @cn_reservation.lab, name: @cn_reservation.name, number_of_samples: @cn_reservation.number_of_samples, start_time: @cn_reservation.start_time } }
     assert_redirected_to cn_reservation_url(@cn_reservation)
   end
 
-  test "should destroy cn_reservation" do
+  test "should destroy cn_reservation when logged in as admin" do
+    log_in_as("password")
     assert_difference('CnReservation.count', -1) do
       delete cn_reservation_url(@cn_reservation)
     end
--- test/controllers/icp_reservations_controller_test.rb
+++ test/controllers/icp_reservations_controller_test.rb
@@ -1,21 +1,30 @@
 require 'test_helper'
 
 class IcpReservationsControllerTest < ActionDispatch::IntegrationTest
-  setup do
+
+  def setup
     @icp_reservation = icp_reservations(:one)
   end
 
-  test "should get index" do
-    get icp_reservations_url
+  test "should get index when not logged in" do
+    get icp_reservations_path
+    assert_redirected_to login_url
+  end
+
+  test "should get index when logged in" do
+    log_in_as("password")
+    get icp_reservations_path
     assert_response :success
   end
 
-  test "should get new" do
-    get new_icp_reservation_url
+  test "should get new when logged in as admin " do
+    log_in_as("password")
+    get new_icp_reservation_path
     assert_response :success
   end
 
   test "should create icp_reservation" do
+    log_in_as("password")
     assert_difference('IcpReservation.count') do
       post icp_reservations_url, params: { icp_reservation: { end_time: @icp_reservation.end_time, information: @icp_reservation.information, lab: @icp_reservation.lab, name: @icp_reservation.name, number_of_samples: @icp_reservation.number_of_samples, start_time: @icp_reservation.start_time } }
     end
@@ -24,21 +33,25 @@ class IcpReservationsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should show icp_reservation" do
+    log_in_as("password")
     get icp_reservation_url(@icp_reservation)
     assert_response :success
   end
 
-  test "should get edit" do
+  test "should get edit when logged in as admin" do
+    log_in_as("password")
     get edit_icp_reservation_url(@icp_reservation)
     assert_response :success
   end
 
   test "should update icp_reservation" do
+    log_in_as("password")
     patch icp_reservation_url(@icp_reservation), params: { icp_reservation: { end_time: @icp_reservation.end_time, information: @icp_reservation.information, lab: @icp_reservation.lab, name: @icp_reservation.name, number_of_samples: @icp_reservation.number_of_samples, start_time: @icp_reservation.start_time } }
     assert_redirected_to icp_reservation_url(@icp_reservation)
   end
 
-  test "should destroy icp_reservation" do
+  test "should destroy icp_reservation when logged in as admin" do
+    log_in_as("password")
     assert_difference('IcpReservation.count', -1) do
       delete icp_reservation_url(@icp_reservation)
     end
